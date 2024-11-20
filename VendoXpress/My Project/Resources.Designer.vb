c057e5bf201ffc2f23dd4516a5ef8a552fa37b84
nicka <nicka@DAVE>
2024-11-20 22:08:53 +0800

    UI Adjstmnt

--- VendoXpress/My Project/Resources.Designer.vb
+++ VendoXpress/My Project/Resources.Designer.vb
@@ -453,16 +453,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Public ReadOnly Property printingOptionFormText() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("printingOptionFormText", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Public ReadOnly Property Refetch() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Refetch", resourceCulture)
